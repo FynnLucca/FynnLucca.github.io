84f6ea7df6984728879c2f526a4ec4ec31939e52
Christian Kanja <christian.kanja@glueckkanja.com>
2018-01-02 16:32:48 +0100

    arduino fertig

--- arduino.c
+++ arduino.c
@@ -30,26 +30,38 @@ void loop() {
 
 void process(BridgeClient client) {
   String command = client.readString();
-  client.write("<html>");
-  client.write("<head>");
-  client.write("<title>Arduino on/off site</title>");
-  client.write("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css' integrity='sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy' crossorigin='anonymous'>");
-  client.write("<stlye>");
-  client.write("a {position:absolute;top:50%;left:50%;transform:translateX(-50%) translateY(-50%);}");
-  client.write("</stlye>");
-  client.write("</head>");
+
+  client.println("Status: 200");
+  client.println("Content-type: text/html");
+  // any other header
+  client.println(); //mandatory blank line
+
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<head>");
+  client.println("<title>Arduino on/off site</title>");
+  client.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css' integrity='sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy' crossorigin='anonymous'>");
+  client.println("<style>");
+  client.println("a {position:absolute;top:50%;left:50%;transform:translateX(-50%) translateY(-50%);}");
+  client.println("</style>");
+  client.println("</head>");
   if (command.startsWith("on")) {
     digitalWrite(7,HIGH);
-    client.write("<a href='http://172.27.2.30/arduino/off' target=_blank><button type='button' class='btn btn-danger btn-lg'>Off</button></a>");
+    client.println("<a href='/arduino/off'><button type='button' class='btn btn-danger btn-lg'>Off</button></a>");
   }
 
   else if (command.startsWith("off")) {
     digitalWrite(7,LOW);
-    client.write("<a href='http://172.27.2.30/arduino/on' target=_blank><button type='button' class='btn btn-success btn-lg'>On</button></a>");
+    client.println("<a href='/arduino/on'><button type='button' class='btn btn-success btn-lg'>On</button></a>");
   }
   else {
-    client.print("Du bist Kacke, es geht nur On/Off");
+    if (digitalRead(7)) {
+      client.println("<a href='/arduino/off'><button type='button' class='btn btn-danger btn-lg'>Off</button></a>");
+    }
+    else {
+      client.println("<a href='/arduino/on'><button type='button' class='btn btn-success btn-lg'>On</button></a>");
+    }
   }
-  client.write("</body>");
-  client.write("</html>");
+  client.println("</body>");
+  client.println("</html>");
 }
